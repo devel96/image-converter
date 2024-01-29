883fd7ca9b3bd12b554be7cb9e9372eb34b8b676
devel96 <55926214+devel96@users.noreply.github.com>
2024-01-29 12:13:22 +0400

    Update ImageManager.php

--- src/ImageManager.php
+++ src/ImageManager.php
@@ -29,7 +29,7 @@ class ImageManager
 
         // get extensions
         $fromExtension = $this->getFileExtensionFromPath($from);
-        $toExtension = $this->getFileExtensionFromPath($to);
+        $toExtension = $this->getOldFileExtensionFromPath($to);
 
         // set gd function names
         $functionFrom = "imagecreatefrom$fromExtension";
@@ -73,15 +73,51 @@ class ImageManager
      */
     private function getFileExtensionFromPath(string $path): string
     {
-        $type = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+        $type = $this->getTypeFromMimeType($path);
 
-        $type = $type === 'jpg' ? FileType::JPEG : $type;
-
-        if (!$type || !$this->isValidType(strtolower($type))) {
+        if (!$type) {
             throw new InvalidFileTypeException();
         }
 
         return strtolower($type);
+    }
+
+    /**
+     * @param string $path
+     * @return string
+     * @throws InvalidFileTypeException
+     */
+    private function getOldFileExtensionFromPath(string $path): string
+    {
+        $type = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+        $type = $type === 'jpg' ? FileType::JPEG : $type;
+
+        if (!$type || !$this->isValidType($type)) {
+            throw new InvalidFileTypeException();
+        }
+
+        return strtolower($type);
+    }
+
+    private function getTypeFromMimeType(string $path): ?string
+    {
+        // Use finfo to get the MIME type of the file
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+
+        if (!$finfo) {
+            return null;
+        }
+
+        $mimeType = finfo_file($finfo, $path);
+        finfo_close($finfo);
+
+        $mimeTypes = [
+            'image/jpeg' => 'jpeg',
+            'image/png' => 'png',
+            'image/gif' => 'gif',
+        ];
+
+        return $mimeTypes[$mimeType] ?? null;
     }
 
     /**
